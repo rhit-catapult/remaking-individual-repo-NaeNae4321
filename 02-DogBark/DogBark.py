846845b3794fc80368338db05b75451fc933c384
NaeNae4321 <debruler.nl@gmail.com>
2025-07-10 10:51:06 -0400

    worked on DogBark.py

--- 02-DogBark/DogBark.py
+++ 02-DogBark/DogBark.py
@@ -17,34 +17,38 @@ def main():
     pygame.display.set_caption("Text, Sound, and an Image")
 
     # Prepare the image
-    # TODO 1: Create an image with the 2dogs.JPG image
-    # TODO 3: Scale the image to be the size (IMAGE_SIZE, IMAGE_SIZE)
+    image1 = pygame.image.load("2dogs.JPG")
+    image1 = pygame.transform.scale(image1, (IMAGE_SIZE, IMAGE_SIZE))
 
     # Prepare the text caption(s)
-    # TODO 4: Create a font object with a size 28 font.
-    # TODO 5: Render the text "Two Dogs" using the font object (it's like MAKING an image).
+    font1 = pygame.font.SysFont("comicsansms", 28)
+    font2 = pygame.font.SysFont("comicsansms", 60)
+
+    caption1 = font1.render("Two Dogs", True, BLACK)
+    caption2 = font2.render("Boop", True, WHITE)
 
     # Prepare the music
-    # TODO 8: Create a Sound object from the "bark.wav" file.
+    bark = pygame.mixer.Sound("bark.wav")
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
-            # TODO 9: Play the music (bark) if there's a mouse click.
+
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                bark.play()
 
         # Clear the screen and set the screen background
         screen.fill(WHITE)
 
         # Draw the image onto the screen
-        # TODO 2: Draw (blit) the image onto the screen at position (0, 0)
+        screen.blit(image1, (0, 0))
 
         # Draw the text onto the screen
-        # TODO 6: Draw (blit) the text image onto the screen in the middle bottom.
         # Hint: Commands like these might be useful..
         #          screen.get_width(), caption1.get_width(), image1.get_height()
-
-        # TODO 7: On your own, create a new bigger font and in white text place a 'funny' message on top of the image.
+        screen.blit(caption1, (image1.get_width() // 2 - caption1.get_width() // 2, image1.get_height() - 8))
+        screen.blit(caption2, (image1.get_width() // 2 - caption2.get_width() // 2, 20))
 
         # Update the screen
         pygame.display.update()
